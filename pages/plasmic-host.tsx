f887a20b287a95721bc14020675963136b37784f
ketutpuja <puja.exe@gmail.com>
2024-08-27 08:11:21 +0800

    Update plasmic-host.tsx

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/host';
+import ConnectWalletButton from '../components/plasmic/jacktron/ConnectWalletButton';
+
 
 // You can register any code components that you want to use here; see
 // https://docs.plasmic.app/learn/code-components-ref/
@@ -9,6 +11,17 @@ import { PlasmicCanvasHost, registerComponent } from '@plasmicapp/react-web/lib/
 // https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
 
 // registerComponent(...)
+
+
+registerComponent(ConnectWalletButton, {
+  name: "ConnectWalletButton",
+  props: {
+    // Tidak ada props khusus untuk komponen ini karena logikanya internal
+  },
+  
+  // Tentukan bagaimana kode Plasmic yang dihasilkan harus mengimpor komponen ini
+  importPath: './components/ConnectWalletButton',
+});
 
 export default function PlasmicHost() {
   return <PlasmicCanvasHost />;
